9fd8b55da085a8d0347f3915f8c0e39b5dd5439b
Anthony Guerrero <adguerrero1935@gmail.com>
2020-03-27 18:09:07 -0400

    added console.table

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require ("mysql");
 const inquirer = require ("inquirer");
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -73,4 +74,3 @@ function updateEmployees() {
 
 
 
-app.listen(PORT, ()=> console.log(`Listening on PORT: ${PORT}`));
